427f1db97be8f59069944aaae97c4960cece8991
IgorPacheco1 <igopacheco7@gmail.com>
2026-04-04 17:59:09 -0300

    FIX: correção de erros no package

--- easy-finance/src/main/java/org/example/ConexaoDB.java
+++ easy-finance/src/main/java/org/example/ConexaoDB.java
@@ -1,19 +1,17 @@
-package org.exemple;
-
+package org.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoDB {
-    private static final  String URL = "jdbc:sqlite:data/easy-fincance.db";
+    private static final String URL = "jdbc:sqlite:data/easy-finance.db";
 
-}
-
-public static Connection conectar(){
-    try {
-        return DriveManager.getConnection(URL);
-    }catch (SQLExeception e) {
-        System.out.println("Erro ao conectar no banco" + e.getMessage());
-        return null;
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(URL);
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar no banco: " + e.getMessage());
+            return null;
+        }
     }
 }
