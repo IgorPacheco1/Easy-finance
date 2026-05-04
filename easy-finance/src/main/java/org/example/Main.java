088217790d18e91f6dfe4b48557635a04436d2d0
IgorPacheco1 <igopacheco7@gmail.com>
2026-05-04 11:03:38 -0300

    Fix: Corrigindo mais erros de sintaxe para os testes

--- easy-finance/src/main/java/org/example/Main.java
+++ easy-finance/src/main/java/org/example/Main.java
@@ -1,25 +1,23 @@
 package org.example;
-//import org.sqlite.JDBC;
+
+import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
 
-        var conexao = ConexaoDB.conectar();
+        Connection conexao = ConexaoDB.conectar();
+
         if (conexao != null) {
-            System.out.println("deu bom");
+            System.out.println("✅ Conexão estabelecida com sucesso!");
+            Transacao novoGasto = new Transacao("Lanche", 25.50, "2026-05-04");
+            ConexaoDB.salvarTransacao(novoGasto);
             try {
                 conexao.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         } else {
-            System.out.println("deu ruim");
+            System.out.println("❌ Falha na conexão. Verifique o banco de dados.");
         }
-
-
-
-        Transacao novoGasto = new Transacao("Lanche", 25.50, "2026-05-04");
-
-
-        ConexaoDB.salvarTransacao(novoGasto);
-
+    }
+}
